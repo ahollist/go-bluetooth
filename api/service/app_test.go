41ae738287b190feef35e54964c02b5fcfa90c3f
Andrew Hollister <ahollister@plantiga.com>
2021-05-31 10:25:18 -0700

    remove extra / in dbuspath in test

--- api/service/app_test.go
+++ api/service/app_test.go
@@ -95,7 +95,7 @@ func TestAppPassCodePersistsWithCustomAgent(t *testing.T) {
 	defer a.Close()
 
 	appAgent := a.Agent()
-	fullPath := dbus.ObjectPath(fmt.Sprintf("%s/%s", "org/bluez/hci0", string(appAgent.Path())))
+	fullPath := dbus.ObjectPath(fmt.Sprintf("%s%s", "org/bluez/hci0", string(appAgent.Path())))
 	appPinCode, err2 := appAgent.RequestPinCode(fullPath)
 	if err2 != nil {
 		t.Fatal(err2)
